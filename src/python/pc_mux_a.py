a9ea7b31bc7f4d7b2df0e539438aafc37d778bee
Grant Slape <me@grantslape.com>
2018-04-01 22:11:07 -0500

    add dummy pc_mux_a verilog cosimulation

--- src/python/pc_mux_a.py
+++ src/python/pc_mux_a.py
@@ -1,4 +1,6 @@
-from myhdl import always_comb
+import os
+
+from myhdl import always_comb, Cosimulation
 
 
 def pc_mux_a(pc_src, imm_jmp_addr, nxt_pc, nxt_inst):
@@ -19,3 +21,22 @@ def pc_mux_a(pc_src, imm_jmp_addr, nxt_pc, nxt_inst):
             nxt_inst.next = nxt_pc
 
     return logic
+
+
+def pc_mux_a_v(pc_src, imm_jmp_addr, nxt_pc, nxt_inst):
+    """
+    Instantiate Verilog module
+    :param pc_src: Which instruction to use, 0 for nxt_pc, 1, for imm_jmp_addr.
+    :param imm_jmp_addr: a new PC value from the EX/MEM register
+    :param nxt_pc: sequential next PC from pc_adder
+    :param nxt_inst: actual next instruction address to pc_mux_b
+    :return: Cosimulation
+    """
+    cmd = "iverilog -o pc_mux_a.out src/verilog/pc_mux_a.v pc_mux_a_tb.v"
+    os.system(cmd)
+
+    return Cosimulation("vvp -m lib/cosimulation/myhdl.vpi pc_mux_a.out",
+                        pc_src=pc_src,
+                        imm_jmp_addr=imm_jmp_addr,
+                        nxt_pc=nxt_pc,
+                        nxt_inst=nxt_inst)
