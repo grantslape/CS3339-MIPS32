345a381eb37d4cbc05276592ae3a0723c08e6f6c
TitansMM <37827201+TitansMM@users.noreply.github.com>
2018-04-02 22:40:41 -0500

    Add files via upload

--- src/python/shift_unit.py
+++ src/python/shift_unit.py
@@ -2,28 +2,31 @@ import os
 
 from myhdl import always_comb, Cosimulation
 
+
 def shift_unit(imm_in, imm_out):
-	"""
-	Shift Unit
-	:param imm_in: The sign-extended 32 bit immediate values
-	:return: The sign-extended value has been shifted left two bits.
-	"""
-	
-	@always_comb
-	def logic():
-		imm_out = imm_in << 2
-	
-	return logic
+    """
+    Shift Unit
+    :param imm_in: The sign-extended 32 bit immediate values
+    :param imm_out: The sign extended value has been shifted left two bits
+    :return: Shift unit module logic
+    """
+
+        @always_comb
+        def logic():
+            imm_out.next = imm_in << 2
+
+        return logic
 
 def shift_unit_v(imm_in, imm_out):
-	"""
-	:param imm_in: The sign-extended 32 bit immediate values
-	:return: The sign-extended value has been shifted left two bits.
-	"""
-        
-	cmd = "iverilog -o shift_unit.out shift_unit.v shift_unit_tb.v"
-	os.System(cmd)
+    """
+    :param imm_in: The sign-extended 32 bit immediate values
+    :param imm_out: The sign extended value has been shifted left two bits
+    :return:A cosimulation object 
+    """
+
+        cmd = "iverilog -o shift_unit.out src/verilog/shift_unit.v src/verilog/shift_unit_tb.v"
+        os.system(cmd)
 	
-	return Cosimulation("vvp -m ./myhdl.vpi shift_unit.out",
-			imm_in=imm_in,
-			imm_out=imm_out)
+        return Cosimulation("vvp -m ./myhdl.vpi shift_unit.out",
+                            imm_in=imm_in,
+                            imm_out=imm_out)
