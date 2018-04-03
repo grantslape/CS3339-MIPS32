1cffab91cc742ff264bf44b30f20449e734bff71
Grant Slape <me@grantslape.com>
2018-04-03 15:36:03 -0500

    add width as constant (32 bits)

--- src/python/settings.py
+++ src/python/settings.py
@@ -3,5 +3,6 @@ settings = {
     'DEFAULT_TEST_LENGTH': 10000,
     'UNSIGNED_MAX_VALUE': 2 ** 32 - 1,
     'SIGNED_MAX_VALUE': 2 ** 31 - 1,
-    'SIGNED_MIN_VALUE': -2 ** 31
+    'SIGNED_MIN_VALUE': -2 ** 31,
+    'WIDTH': 32
 }
