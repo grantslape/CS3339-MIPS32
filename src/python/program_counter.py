3935b93d4e2f19774bf1cb7825b75de7829c54f3
= <=>
2018-04-02 22:03:54 -0500

    Remove conversion file and add Program counter.

---
+++ src/python/program_counter.py
@@ -0,0 +1,22 @@
+#input clock: system clock
+#input [31:0] next_instruction
+#output [31:0] cur_pc: next instruction address
+
+import os
+from myhdl import Cosimulation, always
+
+def program_counter(clock, nxt_inst, cur_pc):
+	@always(clock.posedge)
+	def seq_logic():
+		cur_pc.next = nxt_inst
+	return seq_logic
+
+def program_counter_v(clock, reset, nxt_inst, cur_pc):
+	cmd = "iverilog -o program_counter_v.out src/verilog/program_counter.v src/verilog/tb_program_counter.v"
+	os.system(cmd)
+	
+	return Cosimulation("vvp -m lib/myhdl.vpi program_counter.out", 
+			clock = clock,
+			reset = rest,
+			nxt_inst = nxt_inst,
+			cur_pc=cur_pc)
