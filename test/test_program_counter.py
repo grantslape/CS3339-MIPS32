451f155d80569d0e63da7b7096b554517d7f3415
Grant Slape <me@grantslape.com>
2018-04-04 14:51:09 -0500

    refactor program counter

--- test/test_program_counter.py
+++ test/test_program_counter.py
@@ -11,96 +11,73 @@ from settings import settings as sf
 from clock import clock_gen
 
 
-def setup():
-    clock, pc_write = [Signal(intbv(0)[1:]) for i in range(2)]
-    cur_pc, nxt_inst = [Signal(intbv(0)[32:]) for i in range(2)]
-    return clock, pc_write, nxt_inst, cur_pc
-
-
 @unittest.skip("Program Counter not implemented")
 class TestNormalOperation(TestCase):
-    """Test normal in and out of program counter"""
-    def bench(self, clock, pc_write, nxt_inst, cur_pc):
-        pc_write.next = intbv(0)[1:]
+    """Test program counter"""
+    def setUp(self):
+        self.clock, self.pc_write = [Signal(intbv(0)[1:]) for i in range(2)]
+        self.cur_pc_v, self.cur_pc, self.nxt_inst = [Signal(intbv(0)[32:]) for i in range(3)]
+        self.dut = program_counter(self.clock, self.pc_write, self.nxt_inst, self.cur_pc)
+        self.dut_v = program_counter_v(self.clock, self.pc_write, self.nxt_inst, self.cur_pc_v)
+
+    def normalOp(self, cur_pc):
+        self.pc_write.next = intbv(0)[1:]
         for i in range(sf['DEFAULT_TEST_LENGTH']):
-            nxt_inst.next = intbv(randint(0, sf['MAX_UNSIGNED_VALUE']))[32:]
-            yield posedge(clock)
-            self.assertEqual(cur_pc, nxt_inst)
+            self.nxt_inst.next = intbv(randint(0, sf['MAX_UNSIGNED_VALUE']))[32:]
+            yield posedge(self.clock)
+            self.assertEqual(cur_pc, self.nxt_inst)
+        raise StopSimulation
+
+    def stall(self, cur_pc):
+        self.pc_write.next = intbv(0)[1:]
+        for i in range(sf['DEFAULT_TEST_LENGTH'] / 2):
+            self.nxt_inst.next = intbv(randint(0, sf['MAX_UNSIGNED_VALUE']))[32:]
+            yield posedge(self.clock)
+        self.pc_write.next = intbv(1)[1:]
+        for i in range(sf['DEFAULT_TEST_LENGTH'] / 2):
+            self.assertEqual(self.nxt_inst, cur_pc)
         raise StopSimulation
 
     def testNormalOperationPython(self):
         """Checking normal PC operation Python"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        CLK = clock_gen(clock)
-        dut = program_counter(clock, pc_write, nxt_inst, cur_pc)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
-
-        Simulation(CLK, dut, stim).run(quiet=1)
+        CLK = clock_gen(self.clock)
+        stim = self.normalOp(self.cur_pc)
+        Simulation(CLK, self.dut, stim).run(quiet=1)
 
     def testNormalOperationVerilog(self):
         """Checking normal PC operation Verilog"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        CLK = clock_gen(clock)
-        dut = program_counter_v(clock, pc_write, nxt_inst, cur_pc)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
-
-        Simulation(CLK, dut, stim).run(quiet=1)
+        CLK = clock_gen(self.clock)
+        stim = self.normalOp(self.cur_pc)
+        Simulation(CLK, self.dut_v, stim).run(quiet=1)
 
     def testNormalOperationTogether(self):
         """Checking normal PC operation together"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        cur_pc_v = Signal(intbv(0)[32:])
-        CLK = clock_gen(clock)
-        dut = program_counter(clock, pc_write, nxt_inst, cur_pc)
-        dut_v = program_counter_v(clock, pc_write, nxt_inst, cur_pc_v)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
-        stim_v = self.bench(clock, pc_write, nxt_inst, cur_pc_v)
-
-        Simulation(CLK, dut, dut_v, stim, stim_v).run(quiet=1)
-
-
-@unittest.skip("Program Counter not implemented")
-class TestStallOperation(TestCase):
-    """Test when stall line is asserted"""
-    def bench(self, clock, pc_write, nxt_inst, cur_pc):
-        pc_write.next = intbv(0)[1:]
-        for i in range(sf['DEFAULT_TEST_LENGTH'] / 2):
-            nxt_inst.next = intbv(randint(0, sf['MAX_UNSIGNED_VALUE']))[32:]
-            yield posedge(clock)
-        pc_write.next = intbv(0)[1:]
-        for i in range(sf['DEFAULT_TEST_LENGTH'] / 2):
-            self.assertEqual(nxt_inst, cur_pc)
-        raise StopSimulation
+        CLK = clock_gen(self.clock)
+        stim = self.normalOp(self.cur_pc)
+        stim_v = self.normalOp(self.cur_pc_v)
+        Simulation(CLK, self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
     def testStallOperationPython(self):
         """Test when stall line is asserted Python"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        CLK = clock_gen(clock)
-        dut = program_counter(clock, pc_write, nxt_inst, cur_pc)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
+        CLK = clock_gen(self.clock)
+        stim = self.stall(self.cur_pc)
 
-        Simulation(CLK, dut, stim).run(quiet=1)
+        Simulation(CLK, self.dut, stim).run(quiet=1)
 
     def testStallOperationVerilog(self):
         """Checking stall line is asserted Verilog"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        CLK = clock_gen(clock)
-        dut = program_counter_v(clock, pc_write, nxt_inst, cur_pc)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
+        CLK = clock_gen(self.clock)
+        stim = self.stall(self.cur_pc)
 
-        Simulation(CLK, dut, stim).run(quiet=1)
+        Simulation(CLK, self.dut, stim).run(quiet=1)
 
     def testStallOperationTogether(self):
         """Checking stall line is asserted together"""
-        clock, pc_write, nxt_inst, cur_pc = setup()
-        cur_pc_v = Signal(intbv(0)[32:])
-        CLK = clock_gen(clock)
-        dut = program_counter(clock, pc_write, nxt_inst, cur_pc)
-        dut_v = program_counter_v(clock, pc_write, nxt_inst, cur_pc_v)
-        stim = self.bench(clock, pc_write, nxt_inst, cur_pc)
-        stim_v = self.bench(clock, pc_write, nxt_inst, cur_pc_v)
+        CLK = clock_gen(self.clock)
+        stim = self.stall(self.cur_pc)
+        stim_v = self.stall(self.cur_pc_v)
 
-        Simulation(CLK, dut, dut_v, stim, stim_v).run(quiet=1)
+        Simulation(CLK, self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
 
 if __name__ == '__main__':
