9067b89f34b7328c17f668bd92488f25ba1c50a3
Grant Slape <me@grantslape.com>
2018-04-04 15:26:19 -0500

    refactor ex_mux

--- test/test_ex_mux.py
+++ test/test_ex_mux.py
@@ -12,153 +12,103 @@ from settings import settings as sf
 HALF_PERIOD = delay(sf['PERIOD'] / 2)
 
 
-def setup():
-    reg_dst = Signal(intbv(0)[1:])
-    rt_in, rd_in, dest = [Signal(intbv(0)[5:]) for i in range(3)]
-    return reg_dst, rt_in, rd_in, dest
-
-
 @unittest.skip("Ex Mux not implemented")
 class TestExMuxDeasserted(TestCase):
-    """Test when reg_dst is deasserted"""
-    def bench(self, reg_dst, rt_in, rd_in, dest):
+    """Test Ex mux"""
+    def setUp(self):
+        self.reg_dst = Signal(intbv(0)[1:])
+        self.rt_in, self.rd_in, self.dest, self.dest_v = [Signal(intbv(0)[5:]) for i in range(4)]
+        self.dut = ex_mux(self.reg_dst, self.rt_in, self.rd_in, self.dest)
+        self.dut_v = ex_mux_v(self.reg_dst, self.rt_in, self.rd_in, self.dest_v)
+
+    def deassert(self, dest):
         for i in range(sf['DEFAULT_TEST_LENGTH']):
-            reg_dst.next = 0
-            rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            while rd_in == rt_in:
-                rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            self.reg_dst.next = 0
+            self.rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            while self.rd_in == self.rt_in:
+                self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
             yield HALF_PERIOD
-            self.assertEqual(dest, rt_in)
-            self.assertNotEquals(dest, rd_in)
+            self.assertEqual(dest, self.rt_in)
+            self.assertNotEquals(dest, self.rd_in)
+        raise StopSimulation
+
+    def asserted(self, dest):
+        for i in range(sf['DEFAULT_TEST_LENGTH']):
+            self.reg_dst.next = 1
+            self.rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            while self.rd_in == self.rt_in:
+                self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            yield HALF_PERIOD
+            self.assertEqual(dest, self.rd_in)
+            self.assertNotEquals(dest, self.rt_in)
+        raise StopSimulation
+
+    def dynamic(self, dest):
+        for i in range(sf['DEFAULT_TEST_LENGTH']):
+            self.reg_dst.next = randint(0, 1)
+            self.rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            while self.rd_in == self.rt_in:
+                self.rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
+            yield HALF_PERIOD
+            if self.reg_dst == 0:
+                self.assertEqual(dest, self.rt_in)
+                self.assertNotEquals(dest, self.rd_in)
+            else:
+                self.assertEqual(self.reg_dst, 1)
+                self.assertEqual(dest, self.rd_in)
+                self.assertNotEquals(dest, self.rt_in)
         raise StopSimulation
 
     def testExMuxDeassertedPython(self):
         """Check that RT is returned when reg_dst deasserted Python"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.deassert(self.dest)
+        Simulation(self.dut, stim).run(quiet=1)
 
     def testExMuxDeassertedVerilog(self):
         """Check that RT is returned when reg_dst deasserted Verilog"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux_v(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.deassert(self.dest_v)
+        Simulation(self.dut_v, stim).run(quiet=1)
 
     def testExMuxDeassertedTogether(self):
         """Check that RT is returned when reg_dst deasserted Together"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dest_v = Signal(intbv(0)[5:])
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        dut_v = ex_mux_v(reg_dst, rt_in, rd_in, dest_v)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-        stim_v = self.bench(reg_dst, rt_in, rd_in, dest_v)
-
-        sim = Simulation(dut, dut_v, stim, stim_v)
-        sim.run(quiet=1)
-
-
-@unittest.skip("Ex Mux not implemented")
-class TestExMuxAsserted(TestCase):
-    """Test when reg_dst is asserted"""
-    def bench(self, reg_dst, rt_in, rd_in, dest):
-        for i in range(sf['DEFAULT_TEST_LENGTH']):
-            reg_dst.next = 1
-            rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            while rd_in == rt_in:
-                rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            yield HALF_PERIOD
-            self.assertEqual(dest, rd_in)
-            self.assertNotEquals(dest, rt_in)
-        raise StopSimulation
+        stim = self.deassert(self.dest)
+        stim_v = self.deassert(self.dest_v)
+        Simulation(self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
     def testExMuxAssertedPython(self):
         """Check that RT is returned when reg_dst Asserted Python"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.asserted(self.dest)
+        Simulation(self.dut, stim).run(quiet=1)
 
     def testExMuxAssertedVerilog(self):
         """Check that RT is returned when reg_dst Asserted Verilog"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux_v(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.asserted(self.dest_v)
+        Simulation(self.dut_v, stim).run(quiet=1)
 
     def testExMuxAssertedTogether(self):
         """Check that RT is returned when reg_dst Asserted Together"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dest_v = Signal(intbv(0)[5:])
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        dut_v = ex_mux_v(reg_dst, rt_in, rd_in, dest_v)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-        stim_v = self.bench(reg_dst, rt_in, rd_in, dest_v)
-
-        sim = Simulation(dut, dut_v, stim, stim_v)
-        sim.run(quiet=1)
-
-
-@unittest.skip("Ex Mux not implemented")
-class TestExMuxDynamic(TestCase):
-    """Test dynamic behavior"""
-    def bench(self, reg_dst, rt_in, rd_in, dest):
-        for i in range(sf['DEFAULT_TEST_LENGTH']):
-            reg_dst.next = randint(0, 1)
-            rt_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            while rd_in == rt_in:
-                rd_in.next = intbv(randint(0, sf['WIDTH']))[5:]
-            yield HALF_PERIOD
-            if reg_dst == 0:
-                self.assertEqual(dest, rt_in)
-                self.assertNotEquals(dest, rd_in)
-            else:
-                self.assertEqual(reg_dst, 1)
-                self.assertEqual(dest, rd_in)
-                self.assertNotEquals(dest, rt_in)
-        raise StopSimulation
+        stim = self.asserted(self.dest)
+        stim_v = self.asserted(self.dest_v)
+        Simulation(self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
     def testExMuxDynamicPython(self):
         """Check dynamic behavior Python"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.dynamic(self.dest)
+        Simulation(self.dut, stim).run(quiet=1)
 
     def testExMuxDynamicVerilog(self):
         """Check dynamic behavior Verilog"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dut = ex_mux_v(reg_dst, rt_in, rd_in, dest)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-
-        sim = Simulation(dut, stim)
-        sim.run(quiet=1)
+        stim = self.dynamic(self.dest_v)
+        Simulation(self.dut_v, stim).run(quiet=1)
 
     def testExMuxDynamicTogether(self):
         """Check dynamic behavior Together"""
-        reg_dst, rt_in, rd_in, dest = setup()
-        dest_v = Signal(intbv(0)[5:])
-        dut = ex_mux(reg_dst, rt_in, rd_in, dest)
-        dut_v = ex_mux_v(reg_dst, rt_in, rd_in, dest_v)
-        stim = self.bench(reg_dst, rt_in, rd_in, dest)
-        stim_v = self.bench(reg_dst, rt_in, rd_in, dest_v)
-
-        sim = Simulation(dut, dut_v, stim, stim_v)
-        sim.run(quiet=1)
+        stim = self.dynamic(self.dest)
+        stim_v = self.dynamic(self.dest_v)
+        Simulation(self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
 
 if __name__ == '__main__':
