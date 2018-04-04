dfa2f520f7f57eb0b10d2d5d9b499e267dda9b10
Grant Slape <me@grantslape.com>
2018-04-04 15:31:54 -0500

    refactor test_pc_adder

--- test/test_pc_adder.py
+++ test/test_pc_adder.py
@@ -12,67 +12,54 @@ from settings import settings as sf
 HALF_PERIOD = delay(sf['PERIOD'] / 2)
 
 
-def setup(j):
-    return [Signal(intbv(0)[32:]) for i in range(j)]
-
-
 @unittest.skip("PC Adder not implemented")
 class TestPcAdderZero(TestCase):
-    def bench(self, cur_pc, nxt_pc, nxt_pc_v=intbv(0)[32:]):
-        cur_pc.next = intbv(0)[32:]
+    """Test PC Adder"""
+    def setUp(self):
+        self.cur_pc, self.nxt_pc, self.nxt_pc_v = [Signal(intbv(0)[32:]) for i in range(3)]
+        self.dut = pc_adder(self.cur_pc, self.nxt_pc)
+        self.dut_v = pc_adder_v(self.cur_pc, self.nxt_pc_v)
+
+    def zeroTest(self):
+        self.cur_pc.next = intbv(0)[32:]
         for i in range(sf['DEFAULT_TEST_LENGTH']):
             yield HALF_PERIOD
-            self.assertEqual(bin(nxt_pc), bin(intbv(0)[32:]))
-            self.assertEqual(bin(nxt_pc_v), bin(intbv(0)[32:]))
+            self.assertEqual(bin(self.nxt_pc), bin(intbv(0)[32:]))
+            self.assertEqual(bin(self.nxt_pc_v), bin(intbv(0)[32:]))
+        raise StopSimulation
+
+    def dynamic(self, nxt_pc):
+        for i in range(sf['DEFAULT_TEST_LENGTH']):
+            self.cur_pc.next = intbv(randint(0, sf['UNSIGNED_MAX_VALUE'] - 4))[32:]
+            yield HALF_PERIOD
+            self.assertEqual(bin(nxt_pc), bin(self.cur_pc + 4))
         raise StopSimulation
 
     def testPcAdderZeroPython(self):
         """test pc adder hold zero Python"""
-        cur_pc, nxt_pc = setup(2)
-        dut = pc_adder(cur_pc, nxt_pc)
-        Simulation(dut, self.bench(cur_pc, nxt_pc)).run(quiet=1)
+        Simulation(self.dut, self.zeroTest()).run(quiet=1)
 
     def testPcAdderZeroVerilog(self):
         """test pc adder hold zero Verilog"""
-        cur_pc, nxt_pc = setup(2)
-        dut = pc_adder_v(cur_pc, nxt_pc)
-        Simulation(dut, self.bench(cur_pc, nxt_pc)).run(quiet=1)
+        Simulation(self.dut_v, self.zeroTest()).run(quiet=1)
 
     def testPcAdderZeroTogether(self):
         """test pc adder hold zero together"""
-        cur_pc, nxt_pc, nxt_pc_v = setup(3)
-        dut = pc_adder(cur_pc, nxt_pc)
-        dut_v = pc_adder_v(cur_pc, nxt_pc_v)
-        Simulation(dut, dut_v, self.bench(cur_pc, nxt_pc, nxt_pc_v=nxt_pc_v)).run(quiet=1)
-
-
-@unittest.skip("PC Adder not implemented")
-class TestPcAdderDynamic(TestCase):
-    def bench(self, cur_pc, nxt_pc):
-        for i in range(sf['DEFAULT_TEST_LENGTH']):
-            cur_pc.next = intbv(randint(0, sf['UNSIGNED_MAX_VALUE'] - 4))[32:]
-            yield HALF_PERIOD
-            self.assertEqual(bin(nxt_pc), bin(cur_pc + 4))
-        raise StopSimulation
+        Simulation(self.dut, self.dut_v, self.zeroTest()).run(quiet=1)
 
     def testPcAdderDynamicPython(self):
         """test pc adder adds 4 Python"""
-        cur_pc, nxt_pc = setup(2)
-        dut = pc_adder(cur_pc, nxt_pc)
-        Simulation(dut, self.bench(cur_pc, nxt_pc)).run(quiet=1)
+        Simulation(self.dut, self.dynamic(self.nxt_pc)).run(quiet=1)
 
     def testPcAdderDynamicVerilog(self):
         """test pc adder adds 4 Verilog"""
-        cur_pc, nxt_pc = setup(2)
-        dut = pc_adder_v(cur_pc, nxt_pc)
-        Simulation(dut, self.bench(cur_pc, nxt_pc)).run(quiet=1)
+        Simulation(self.dut, self.dynamic(self.nxt_pc)).run(quiet=1)
 
     def testPcAdderDynamicTogether(self):
         """test pc adder adds 4 together"""
-        cur_pc, nxt_pc, nxt_pc_v = setup(3)
-        dut = pc_adder(cur_pc, nxt_pc)
-        dut_v = pc_adder_v(cur_pc, nxt_pc_v)
-        Simulation(dut, dut_v, self.bench(cur_pc, nxt_pc), self.bench(cur_pc, nxt_pc_v)).run(quiet=1)
+        stim = self.dynamic(self.nxt_pc)
+        stim_v = self.dynamic(self.nxt_pc_v)
+        Simulation(self.dut, self.dut_v, stim, stim_v).run(quiet=1)
 
 
 if __name__ == '__main__':
