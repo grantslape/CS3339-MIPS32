478df42477a0f52e125594a3981475e378135ec1
Mark Gitthens <mgitthens@gmail.com>
2018-04-04 20:22:17 -0500

    Created a generalized 2:1 32-bit mux module.

---
+++ src/python/32_bit_2_to_1_mux.py
@@ -0,0 +1,41 @@
+import os
+
+from myhdl import always_comb, Cosimulation
+
+def 32_bit_2_to_1_mux(ctrl_line, input1, input2, output):
+	"""
+	32_bit_2_to_1_mux
+
+	:param ctrl_line: Which input should be output. If 1 input2 else input1
+	:param input1: first 32-bit input
+	:param input2: second 32-bit input
+	:param output: 32-bit output
+	:return: generator logic
+	"""
+
+	@always_comb
+	def logic():
+		if ctrl_line == 0:
+			output.next = input1
+		else:
+			output.next = input2
+	return
+
+def 32_bit_2_to_1_mux(ctrl_line, input1, input2, output):
+	"""
+	Instantiate Vefilog module
+
+	:param ctrl_line: Which input should be output. If 1 input2 else input1
+	:param input1: first 32-bit input
+	:param input2: second 32-bit input
+	:param output: 32-bit output
+	:return: Cosimulation 
+	"""
+	cmd = "iverilog -o 32_bit_2_to_1_mux.out src/verilog/32_bit_2_to_1_mux.v src/verilog/32_bit_2_to_1_mux_tb.v"
+	os.system(cmd)
+
+	return Cosimulation("vvp -m lib/myhdl.vpi 32_bit_2_to_1_mux.out",
+			ctrl_line = ctrl_line,
+			input1 = input1,
+			input2 = input2,
+			output = output)	
