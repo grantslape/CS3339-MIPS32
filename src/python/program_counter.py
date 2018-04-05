f00b3480be3d912dc2e663dd38d245539d13a8c6
Mark Gitthens <mgitthens@gmail.com>
2018-04-05 02:50:09 -0500

    Fixed merge issues.

--- src/python/program_counter.py
+++ src/python/program_counter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #input clock: system clock
 #input [31:0] next_instruction
 #output [31:0] cur_pc: next instruction address
@@ -25,33 +24,3 @@ def program_counter_v(clock, reset, nxt_inst, cur_pc):
 			reset = rest,
 			nxt_inst = nxt_inst,
 			cur_pc=cur_pc)
-=======
-import os
-
-from myhdl import always_seq, Cosimulation
-
-
-def program_counter(clock, pc_write, nxt_inst, cur_pc):
-    """
-    Program Counter
-    :param clock: system clock
-    :param pc_write: 1 to stall (repeat instruction), from hazard_unit.pc_write
-    :param nxt_inst: next instruction address from pc_mux_b
-    :param cur_pc: next instruction address goes to inst_mem, pc_mux_c
-    :return: module logic
-    """
-    # NOT IMPLEMENTED
-    pass
-
-
-def program_counter_v(clock, pc_write, nxt_inst, cur_pc):
-    """
-    Program Counter Verilog
-    :param clock: system clock
-    :param pc_write: 1 to stall (repeat instruction), from hazard_unit.pc_write
-    :param nxt_inst: next instruction address from pc_mux_b
-    :param cur_pc: next instruction address goes to inst_mem, pc_mux_c
-    :return: module logic
-    """
-    return Cosimulation()
->>>>>>> master
