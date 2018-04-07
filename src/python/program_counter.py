2c047c4546514f0055f3c20a4b86d58e39c7c44c
Mark Gitthens <mgitthens@gmail.com>
2018-04-06 21:26:12 -0500

    Program counter now passes tests (with slight modifications).

--- src/python/program_counter.py
+++ src/python/program_counter.py
@@ -11,12 +11,12 @@ def program_counter(clock, pc_write, nxt_inst, cur_pc):
 	def seq_logic():
 		if pc_write == 0:
 			cur_pc.next = nxt_inst
-			pc_reg = nxt_inst
-			
+		else:
+			cur_pc.next = cur_pc
 	return seq_logic
 
 def program_counter_v(clock, pc_write, nxt_inst, cur_pc):
-	cmd = "iverilog -o program_counter_v.out src/verilog/program_counter.v src/verilog/tb_program_counter.v"
+	cmd = "iverilog -o program_counter_v.out src/verilog/program_counter.v src/verilog/program_counter_tb.v"
 	os.system(cmd)
 	
 	return Cosimulation("vvp -m lib/myhdl.vpi program_counter.out", 
