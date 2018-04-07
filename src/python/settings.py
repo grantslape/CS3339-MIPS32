5302a3872819bec9f4781fab6fedc2e10cccf2a4
serena-marie <serena-marie@users.noreply.github.com>
2018-04-07 13:21:51 -0500

    signed max & min name didnt match test... am lazy and just added to settings

--- src/python/settings.py
+++ src/python/settings.py
@@ -3,7 +3,9 @@ settings = {
     'DEFAULT_TEST_LENGTH': 10000,
     'UNSIGNED_MAX_VALUE': 2 ** 32 - 1,
     'SIGNED_MAX_VALUE': 2 ** 31 - 1,
+    'MAX_SIGNED_VALUE': 2 ** 31 - 1,
     'SIGNED_MIN_VALUE': -2 ** 31,
+    'MIN_SIGNED_VALUE': -2 ** 31,
     'WIDTH': 32,
     'ACTIVE_LOW': 0,
     'INACTIVE_HIGH': 1
