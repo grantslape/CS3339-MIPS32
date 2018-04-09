6a6e63650cd957b871fb18e0aea294dc8d8b012f
serena-marie <serena-marie@users.noreply.github.com>
2018-04-08 19:53:44 -0500

    updated variable names

--- src/python/mux32bit3to1.py
+++ src/python/mux32bit3to1.py
@@ -2,31 +2,30 @@ import os
 
 from myhdl import always_comb, Cosimulation
 
-def mux32bit3to1(ctrl, data1, mem_rd, wb_rd, output):
+def mux32bit3to1(ctrl_line, data1, mem_rd, wb_rd, out):
     """
     3:1 Mux for forwarding results from 2 cycles ago
-    :param ctrl: two bit input selector from fwd_unit
+    :param ctrl_line: two bit input selector from fwd_unit
     :param data1: rs data received from id_ex 0
     :param mem_rd: rd from the previous cycle, received from ex_mem 01
     :param wb_rd: rd from two cycles ago, received from wb_mux 10
-    :param output: Output for rs/op1.  Sent to ALU
+    :param out: Output for rs/op1.  Sent to ALU
     :return: module logic
     """
-
     @always_comb
     def logic():
-        if ctrl == 2:
+        if ctrl_line == 2:
            #datasrc is the mem/wb pipeline register
-            output.next = wb_rd
-        elif ctrl == 1:
+            out.next = wb_rd
+        elif ctrl_line == 1:
             #datasrc is ex/mem pipeline register
-            output.next = mem_rd
-        elif ctrl == 0:
+            out.next = mem_rd
+        elif ctrl_line == 0:
            #datasrc is the id/ex pipeline register
-            output.next = data1
+            out.next = data1
     return logic
-
-def mux32bit3to1_v(ctrl, data1, mem_rd, wb_rd, output):
+    
+def mux32bit3to1_v(ctrl_line, data1, mem_rd, wb_rd, out):
 
     """
     3:1 Mux for forwarding results from 2 cycles ago Verilog
@@ -41,8 +40,8 @@ def mux32bit3to1_v(ctrl, data1, mem_rd, wb_rd, output):
     cmd = "iverilog -o bin/mux32bit3to1.out src/verilog/mux32bit3to1.v src/verilog/mux32bit3to1_tb.v"
     os.system(cmd)
     return Cosimulation("vvp -m  lib/myhdl.vpi bin/mux32bit3to1.out",
-                        ctrl=ctrl,
+                        ctrl_line=ctrl_line,
                         data1=data1,
                         mem_rd=mem_rd,
                         wb_rd=wb_rd,
-                        output=output)
+                        out=out)
