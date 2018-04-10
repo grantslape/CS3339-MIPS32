37e85b9166bb7a3c50d8cd1adb5cb746c591ad99
Grant Slape <me@grantslape.com>
2018-04-09 23:48:09 -0500

    fix data mem tests

--- test/test_data_mem.py
+++ test/test_data_mem.py
@@ -1,39 +1,40 @@
 """Data Memory Unit Tests"""
 import unittest
 from unittest import TestCase
-from myhdl import Simulation, StopSimulation, posedge
+from myhdl import Simulation, StopSimulation, posedge, Signal
 
-from src.python.program_counter import data_mem, data_mem_v
+from src.python.data_mem import data_mem, data_mem_v
 from src.commons.settings import settings as sf
 from src.commons.clock import clock_gen
-from src.commons.signal_generator import unsigned_signal_set, signed_signal_set, \
-    random_unsigned_intbv, random_signed_intbv
+from src.commons.signal_generator import unsigned_signal_set, random_unsigned_intbv, \
+    random_signed_intbv, unsigned_intbv, signed_intbv
 
 
-@unittest.skip("Data Mem not implemented")
+@unittest.skip("Data memory not implemented")
 class TestDataMem(TestCase):
     """test data memory"""
     def setUp(self):
         self.clock, self.read_ctrl, self.w_ctrl = unsigned_signal_set(3, width=1)
-        self.mem_addr = signed_signal_set(1)
-        self.wdata, self.rdata, self.rdata_v = unsigned_signal_set(3)
-        self.dut = data_mem(clock=self.clock,
-                            read_ctrl=self.read_ctrl,
-                            w_ctrl=self.w_ctrl,
-                            mem_addr=self.mem_addr,
-                            wdata=self.wdata,
-                            rdata=self.rdata)
+        self.mem_addr = Signal(unsigned_intbv())
+        self.rdata, self.rdata_v = unsigned_signal_set(2)
+        self.wdata = Signal(signed_intbv())
+        self.dut = data_mem(clk=self.clock,
+                            address=self.read_ctrl,
+                            write_wire=self.w_ctrl,
+                            read_wire=self.mem_addr,
+                            write_data=self.wdata,
+                            read_data=self.rdata)
 
     def getVerilog(self):
         """
         :return: Verilog design under test
         """
-        return data_mem_v(clock=self.clock,
-                          read_ctrl=self.read_ctrl,
-                          w_ctrl=self.w_ctrl,
-                          mem_addr=self.mem_addr,
-                          wdata=self.wdata,
-                          rdata=self.rdata_v)
+        return data_mem_v(clk=self.clock,
+                          read_wire=self.read_ctrl,
+                          write_wire=self.w_ctrl,
+                          address=self.mem_addr,
+                          write_data=self.wdata,
+                          read_data=self.rdata_v)
 
     # TODO: Find a way to test read and write separately
     def testDynamic(self, rdata):
