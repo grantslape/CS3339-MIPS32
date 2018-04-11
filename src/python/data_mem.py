4f5daeeb07b3ee2d8d999317a5955a155119fc85
Patrick Vinas <pvinas@novetta.com>
2018-04-10 21:43:59 -0500

    data_mem module with 2 working tests (one failing)

---
+++ src/python/data_mem.py
@@ -0,0 +1,62 @@
+"""Data memory module"""
+from os import system
+from array import array
+from myhdl import always, Cosimulation, intbv
+
+def data_mem(clk, address, write_wire, read_wire, write_data, read_data):
+    """
+    main data memory
+    :param clk: clock (input)
+    :param address: 32-bit memory address to access
+    :param write_data: data to write to address
+    :param read_data: data to read from address
+    :param read_wire: activate read from mem
+    :param write_wire: activate write to mem
+    :return: module logic
+    """
+
+    # Initialize RAM from memory_file
+    f = open('memory_file')
+    mem_array = []
+    try:
+        mem_array = [intbv(line) for line in f]    
+    except IOError:
+        pass
+    f.close()
+        
+
+    @always(clk.negedge)
+    def write():
+        mem_addr = address.next
+        if write_wire == 1:
+            mem_array[mem_addr] = write_data
+
+    @always(clk.posedge)
+    def read():
+        mem_addr = address.next
+        if read_wire == 1:
+            read_data.next = mem_array[mem_addr]
+
+    return read, write
+
+
+def data_mem_v(clk, address, write_wire, read_wire, write_data, read_data):
+    """
+    main data memory (verilog)
+    :param clk: clock (input)
+    :param address: 32-bit memory address to access
+    :param write_data: data to write to address
+    :param read_data: data to read from address
+    :param read_wire: activate read from mem
+    :param write_wire: activate write to mem
+    :return: module logic
+    """
+    cmd = "iverilog -o bin/data_mem.out src/verilog/data_mem.v src/verilog/data_mem_tb.v"
+    system(cmd)
+    return Cosimulation("vvp -m ./lib/myhdl.vpi bin/data_mem.out",
+                        clk=clk,
+                        address=address,
+                        write_wire=write_wire,
+                        read_wire=read_wire,
+                        write_data=write_data,
+                        read_data=read_data)
---
+++ src/verilog/data_mem.v
@@ -0,0 +1,50 @@
+`timescale 1ns/10ps
+
+module data_mem(
+        clk, 
+        address, 
+        write_wire, 
+        read_wire, 
+        write_data, 
+        read_data
+);
+
+  // main data memory
+  // clk: clock (input)
+  // address: 32-bit memory address to access
+  // write_data: data to write to address
+  // read_data: data to read from address
+  // read_wire: activate read from mem
+  // write_wire: activate write to mem
+  // return: module logic
+
+  input [0:0] clk;
+  input [31:0] address;
+  input [0:0] write_wire;
+  input [0:0] read_wire;
+  input [31:0] write_data;
+  output reg [31:0] read_data;
+
+  reg [31:0] mem_array [0:1048576];
+
+  //load the buffer with data from the memory file
+  initial
+    $readmemb("memory_file", mem_array);
+
+  always @(negedge clk)
+  begin
+    if ( write_wire == 1 )
+    begin
+      $display("saving %d to %d", write_data, address);
+      mem_array[address] = write_data;
+    end
+  end
+
+  always @(posedge clk)
+  begin
+    if (read_wire == 1)
+    begin
+        read_data = mem_array[address];
+    end
+  end
+endmodule
