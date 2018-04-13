3be3699cb5418561e4504ff7d978b8225ab67dd6
Grant Slape <me@grantslape.com>
2018-04-12 20:41:19 -0500

    add ex_mem to tests

--- test/test_all_modules.py
+++ test/test_all_modules.py
@@ -6,7 +6,7 @@ import unittest
 
 from test import test_fwd_unit, test_shift_unit, test_mux32bit2to1, test_pc_adder, \
     test_mux32bit3to1, test_ex_mux, test_program_counter, test_rfile, test_sign_extender, \
-    test_id_ex, test_id_shift_left, test_branch_adder, test_mem_wb
+    test_id_ex, test_id_shift_left, test_branch_adder, test_mem_wb, test_ex_mem
 #   test_data_mem, test_if_id, test_hazard_unit
 
 MODULES = (test_mux32bit2to1,
@@ -21,7 +21,8 @@ MODULES = (test_mux32bit2to1,
            test_id_ex,
            test_id_shift_left,
            test_branch_adder,
-           test_mem_wb,)
+           test_mem_wb,
+           test_ex_mem,)
 TESTER = unittest.defaultTestLoader
 
 
