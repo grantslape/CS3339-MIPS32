132c737978bf825505d4854f8c157f5a483472dc
Patrick Vinas <pvinas@novetta.com>
2018-04-12 19:52:15 -0500

    Add faster data_mem implementation

--- src/python/data_mem2.py
+++ src/python/data_mem2.py
@@ -1,11 +1,8 @@
 """Data memory module"""
 from os import system
-from myhdl import always, Cosimulation
+from myhdl import always, Cosimulation, intbv
 
-from src.commons.settings import settings as sf
-from src.commons.signal_generator import unsigned_intbv
-
-def data_mem(clk, address, write_wire, read_wire, write_data, read_data):
+def data_mem2(clk, address, write_wire, read_wire, write_data, read_data):
     """
     main data memory
     :param clk: clock (input)
@@ -17,7 +14,14 @@ def data_mem(clk, address, write_wire, read_wire, write_data, read_data):
     :return: module logic
     """
 
-    mem_array = [unsigned_intbv()] * 2**sf['MEMORY_WIDTH']
+    # Initialize RAM from memory_file
+    mem_file = open('memory_file')
+    mem_array = []
+    try:
+        mem_array = [intbv(line) for line in mem_file]
+    except IOError:
+        pass
+    mem_file.close()
 
     @always(clk.posedge)
     def logic():
@@ -29,7 +33,7 @@ def data_mem(clk, address, write_wire, read_wire, write_data, read_data):
     return logic
 
 
-def data_mem_v(clk, address, write_wire, read_wire, write_data, read_data):
+def data_mem2_v(clk, address, write_wire, read_wire, write_data, read_data):
     """
     main data memory (verilog)
     :param clk: clock (input)
@@ -40,9 +44,9 @@ def data_mem_v(clk, address, write_wire, read_wire, write_data, read_data):
     :param write_wire: activate write to mem
     :return: module logic
     """
-    cmd = "iverilog -o bin/data_mem.out src/verilog/data_mem.v src/verilog/data_mem_tb.v"
+    cmd = "iverilog -o bin/data_mem2.out src/verilog/data_mem2.v src/verilog/data_mem_tb.v"
     system(cmd)
-    return Cosimulation("vvp -m ./lib/myhdl.vpi bin/data_mem.out",
+    return Cosimulation("vvp -m ./lib/myhdl.vpi bin/data_mem2.out",
                         clk=clk,
                         address=address,
                         write_wire=write_wire,
