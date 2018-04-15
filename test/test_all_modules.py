28536b919f42d24cb5c2b7a9d5a0e42a1f1efc3c
Natalie Garza <n_g137@txstate.edu>
2018-04-14 19:20:40 -0500

    Hazard unit ready to merge to master

--- test/test_all_modules.py
+++ test/test_all_modules.py
@@ -6,9 +6,9 @@ import unittest
 
 from test import test_fwd_unit, test_shift_unit, test_mux32bit2to1, test_pc_adder, \
     test_mux32bit3to1, test_ex_mux, test_program_counter, test_rfile, test_sign_extender, \
-    test_id_ex, test_id_shift_left, test_branch_adder, test_mem_wb, test_ex_mem, test_data_mem, \
+    test_id_ex, test_branch_adder, test_mem_wb, test_ex_mem, test_data_mem, \
     test_branch_unit, test_inst_mem, test_hazard_unit
-#   test_if_id
+#   test_if_id, , test_id_shift_left
 
 MODULES = (test_mux32bit2to1,
            test_mux32bit3to1,
@@ -20,7 +20,6 @@ MODULES = (test_mux32bit2to1,
            test_shift_unit,
            test_sign_extender,
            test_id_ex,
-           test_id_shift_left,
            test_branch_adder,
            test_mem_wb,
            test_ex_mem,
