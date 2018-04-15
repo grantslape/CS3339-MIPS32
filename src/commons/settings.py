f96c4263f26b3c54087af69e0e80e56017f64e17
Grant Slape <me@grantslape.com>
2018-04-14 21:10:19 -0500

    reduce default test length to 5000

--- src/commons/settings.py
+++ src/commons/settings.py
@@ -1,6 +1,6 @@
 settings = {
     'PERIOD': 10,
-    'DEFAULT_TEST_LENGTH': 10000,
+    'DEFAULT_TEST_LENGTH': 5000,
     'UNSIGNED_MAX_VALUE': 2 ** 32 - 1,
     'SIGNED_MAX_VALUE': 2 ** 31 - 1,
     'SIGNED_MIN_VALUE': -2 ** 31,
