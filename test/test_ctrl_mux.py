e4d8fcf859956460fdfd596aa3885c76cccc3f2d
Grant Slape <me@grantslape.com>
2018-04-16 12:37:57 -0500

    finished ctrl_mux tests

--- test/test_ctrl_mux.py
+++ test/test_ctrl_mux.py
@@ -6,7 +6,7 @@ from myhdl import Simulation, StopSimulation, ResetSignal, intbv, Signal
 from src.python.ctrl_mux import ctrl_mux, ctrl_mux_v
 from src.commons.clock import half_period
 from src.commons.settings import settings as sf
-from src.commons.signal_generator import unsigned_signal_set
+from src.commons.signal_generator import unsigned_signal_set, random_unsigned_intbv
 
 
 @unittest.skip("Ctrl mux not implemented")
@@ -18,7 +18,7 @@ class TestControlMux(TestCase):
         self.jump = Signal(intbv()[2:])
         self.branch, self.mem_read, self.mem_to_reg, self.mem_write, self.alu_src, self.reg_write, \
             self.reg_dst, self.ex_stall = unsigned_signal_set(8, width=1)
-        self.alu_op, self.alu_op_v = unsigned_signal_set(2, width=sf['ALU_CODE_SIZE'])
+        self.alu_op = Signal(intbv()[sf['ALU_CODE_SIZE']])
 
         # OUTPUTS
         self.jump_out, self.jump_out_v = unsigned_signal_set(2, width=2)
@@ -59,25 +59,46 @@ class TestControlMux(TestCase):
         else:
             return ctrl_mux_v(**self.get_args(which="verilog"))
 
+    def random_input(self):
+        """Randomize input signals for test cases"""
+        self.jump = random_unsigned_intbv(width=2)
+        self.branch, self.mem_read, self.mem_to_reg, self.mem_write, self.alu_src, self.reg_write, \
+            self.reg_dst = unsigned_signal_set(7, width=1)
+        self.alu_op = random_unsigned_intbv(width=sf['ALU_CODE_SIZE'])
+
     def deassert(self, python=False, verilog=False):
         """test normal functionality"""
         self.ex_stall.next = 0
         for _ in range(sf['DEFAULT_TEST_LENGTH']):
-            # set random values
+            self.random_input()
             yield half_period()
             if python:
-                # assert some stuff
-                pass
+                self.assertEqual(self.jump, self.jump_out)
+                self.assertEqual(self.branch, self.branch_out)
+                self.assertEqual(self.mem_read, self.mem_read_out)
+                self.assertEqual(self.mem_to_reg, self.mem_to_reg_out)
+                self.assertEqual(self.mem_write, self.mem_write_out)
+                self.assertEqual(self.alu_src, self.alu_src_out)
+                self.assertEqual(self.reg_write, self.reg_write_out)
+                self.assertEqual(self.reg_dst, self.reg_dst_out)
+                self.assertEqual(self.alu_op, self.alu_op_out)
             if verilog:
-                # assert some more stuff
-                pass
+                self.assertEqual(self.jump, self.jump_out_v)
+                self.assertEqual(self.branch, self.branch_out_v)
+                self.assertEqual(self.mem_read, self.mem_read_out_v)
+                self.assertEqual(self.mem_to_reg, self.mem_to_reg_out_v)
+                self.assertEqual(self.mem_write, self.mem_write_out_v)
+                self.assertEqual(self.alu_src, self.alu_src_out_v)
+                self.assertEqual(self.reg_write, self.reg_write_out_v)
+                self.assertEqual(self.reg_dst, self.reg_dst_out_v)
+                self.assertEqual(self.alu_op, self.alu_op_out_v)
         raise StopSimulation
 
     def asserted(self, python=False, verilog=False):
         """test stalled functionality"""
         self.ex_stall.next = 1
         for _ in range(sf['DEFAULT_TEST_LENGTH']):
-            # set random values
+            self.random_input()
             yield half_period()
             if python:
                 self.assertEqual(0, self.jump_out)
@@ -101,6 +122,44 @@ class TestControlMux(TestCase):
                 self.assertEqual(0, self.alu_op_out_v)
                 pass
         raise StopSimulation
+
+    def testNormalOperationPython(self):
+        """testing normal operations python"""
+        stim = self.deassert(python=True)
+        dut = self.get_module()
+        Simulation(dut, stim).run(quiet=1)
+
+    def testNormalOperationVerilog(self):
+        """testing normal operations verilog"""
+        stim = self.deassert(verilog=True)
+        dut_v = self.get_module(which="verilog")
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testNormalOperationTogether(self):
+        """testing normal operations together"""
+        stim = self.deassert(verilog=True, python=True)
+        dut = self.get_module()
+        dut_v = self.get_module(which="verilog")
+        Simulation(stim, dut_v, dut).run(quiet=1)
+
+    def testStallOperationPython(self):
+        """testing Stall operations python"""
+        stim = self.asserted(python=True)
+        dut = self.get_module()
+        Simulation(dut, stim).run(quiet=1)
+
+    def testStallOperationVerilog(self):
+        """testing Stall operations verilog"""
+        stim = self.asserted(verilog=True)
+        dut_v = self.get_module(which="verilog")
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testStallOperationTogether(self):
+        """testing Stall operations together"""
+        stim = self.asserted(verilog=True, python=True)
+        dut = self.get_module()
+        dut_v = self.get_module(which="verilog")
+        Simulation(stim, dut_v, dut).run(quiet=1)
 
 
 if __name__ == '__main__':
