6f4c6302175ac74dab72d0e34830d280a3ac24bc
Natalie Garza <n_g137@txstate.edu>
2018-04-15 22:38:21 -0500

    if id verilog files generated, still need to test...

--- src/python/if_id.py
+++ src/python/if_id.py
@@ -1,5 +1,5 @@
 import os
-from myhdl import always_comb, Cosimulation
+from myhdl import always, Cosimulation
 
 def if_id(clock, if_id_write, nxt_pc, inst_in, op_code, rs, rt, imm, rd, funct_out, pc_out, top4, target_out):
     """
@@ -17,71 +17,39 @@ def if_id(clock, if_id_write, nxt_pc, inst_in, op_code, rs, rt, imm, rd, funct_o
     :param top4: top 4 bits of nxt_pc. From pc_adder
     :param target_out: 26 bit jump immediate
     """
-    # COULD NOT FIND SUBI IN THE MIPS OPCODE MAP ON A-50 IN BOOK
-    @always_comb
-    def PC_logic():
-        if if_id_write == 1:
-            # program counter logic
-            pc_out.next = pc_out
-            top4.next = top4
-        else:
-            # program counter logic
-            pc_out.next = nxt_pc
-            top4.next = nxt_pc[:28]
-
-
-    @always_comb
-    def INSTR_logic():
+    @always(clock.posedge)
+    def logic():
         # if if_id_write is true then do not update the state Register
         if if_id_write == 1:
-            op_code.next = op_code
-            # if r-type: add, sub, xor, jr
-            if op_code == 0:
-                rs.next = rs
-                rt.next = rt
-                rd.next = rd
-                funct_out.next = funct_out
-            # if i-type: addi, beq, lw, sw
-            elif op_code == 8 or op_code == 4 or op_code == 35 or op_code == 43:
-                rs.next = rs
-                rt.next = rt
-                imm.next = imm
-            # if j-type: j, jal
-            elif op_code == 2 or op_code == 3:
-                target_out.next = target_out
-            # don't know what to do when opcode not recognized
-            # in this im going to assume mystery opcode is
-            # the missing subi instruction B-)
-            else:
-                rs.next = rs
-                rt.next = rt
-                imm.next = imm
+            # program counter logic
+            pc_out[:].next = pc_out
+            top4[:].next = top4
+
+            # instruction logic
+            op_code[:].next = op_code
+            rs[:].next = rs
+            rt[:].next = rt
+            rd[:].next = rd
+            funct_out[:].next = funct_out
+            imm[:].next = imm
+            target_out[:].next = target_out
+
         # else update the register with the appropriate inputs
         else:
-            op_code.next = op_code[:26]
-            # if r-type: add, sub, xor, jr
-            if op_code == 0:
-                rs.next = inst_in[26:21]
-                rt.next = inst_in[21:16]
-                rd.next = inst_in[16:11]
-                funct_out.next = inst_in[6:0]
-            # if i-type: addi, beq, lw, sw
-            elif op_code == 8 or op_code == 4 or op_code == 35 or op_code == 43:
-                rs.next = inst_in[26:21]
-                rt.next = inst_in[21:16]
-                imm.next = inst_in[16:0]
-            # if j-type: j, jal
-            elif op_code == 2 or op_code == 3:
-                target_out.next = inst_in[26:0]
-            # don't know what to do when opcode not recognized
-            # in this im going to assume mystery opcode is
-            # the missing subi instruction
-            else:
-                rs.next = inst_in[26:21]
-                rt.next = inst_in[21:16]
-                imm.next = inst_in[16:0]
+            # program counter logic
+            pc_out[:].next = nxt_pc
+            top4[:].next = nxt_pc[:28]
 
-    return PC_logic, INSTR_logic
+            # instruction logic
+            op_code[:].next = op_code[:26]
+            rs[:].next = inst_in[26:21]
+            rt[:].next = inst_in[21:16]
+            rd[:].next = inst_in[16:11]
+            funct_out[:].next = inst_in[6:0]
+            imm[:].next = inst_in[16:0]
+            target_out[:].next = inst_in[26:0]
+
+    return logic
 
 
 def if_id_v(clock, if_id_write, nxt_pc, inst_in, op_code, rs, rt, imm, rd, funct_out, pc_out, top4, target_out):
@@ -105,6 +73,7 @@ def if_id_v(clock, if_id_write, nxt_pc, inst_in, op_code, rs, rt, imm, rd, funct
     os.system(cmd)
 
     return Cosimulation("vvp -m ./lib/myhdl.vpi bin/if_id.out",
+                        clock=clock,
                         if_id_write=if_id_write,
                         nxt_pc=nxt_pc,
                         inst_in=inst_in,
@@ -116,5 +85,4 @@ def if_id_v(clock, if_id_write, nxt_pc, inst_in, op_code, rs, rt, imm, rd, funct
                         funct_out=funct_out,
                         pc_out=pc_out,
                         top4=top4,
-                        target_out=target_out,
-                        clock=clock)
+                        target_out=target_out)
---
+++ src/verilog/if_id_tb.v
@@ -0,0 +1,53 @@
+module tb_if_id;
+
+reg clock;
+reg if_id_write;
+reg [31:0] nxt_pc;
+reg [31:0] inst_in;
+wire [5:0] op_code;
+wire [4:0] rs;
+wire [4:0] rt;
+wire [15:0] imm;
+wire [4:0] rd;
+wire [5:0] funct_out;
+wire [31:0] pc_out;
+wire [3:0] top4;
+wire [25:0] target_out;
+
+initial begin
+    $from_myhdl(
+        clock,
+        if_id_write,
+        nxt_pc,
+        inst_in
+    );
+    $to_myhdl(
+        op_code,
+        rs,
+        rt,
+        imm,
+        rd,
+        funct_out,
+        pc_out,
+        top4,
+        target_out
+    );
+end
+
+if_id dut(
+    clock,
+    if_id_write,
+    nxt_pc,
+    inst_in,
+    op_code,
+    rs,
+    rt,
+    imm,
+    rd,
+    funct_out,
+    pc_out,
+    top4,
+    target_out
+);
+
+endmodule
