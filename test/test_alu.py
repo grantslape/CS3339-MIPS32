5a9d30043daff13cef627d3dbc011d577c1aa844
Grant Slape <me@grantslape.com>
2018-04-17 18:17:19 -0500

    alu tests

--- test/test_alu.py
+++ test/test_alu.py
@@ -6,7 +6,7 @@ from myhdl import Simulation, StopSimulation, intbv, Signal
 from src.python.alu import alu, alu_v
 from src.commons.settings import settings as sf
 from src.commons.clock import half_period
-from src.commons.signal_generator import signed_signal_set, unsigned_signal_set, random_signed_intbv, \
+from src.commons.signal_generator import signed_signal_set, unsigned_signal_set, \
     rand_signed_signal_set
 
 
@@ -34,6 +34,7 @@ class TestALU(TestCase):
                      result=self.result_v)
 
     def check_zero(self, zero_flag, result):
+        """Check if zero flag is set"""
         if result == 0:
             self.assertEqual(1, zero_flag)
         else:
@@ -110,10 +111,12 @@ class TestALU(TestCase):
 
     @unittest.skip("SHIFT NOT IMPLEMENTED")
     def shift_left_test(self):
+        """stim for SLL"""
         pass
 
     @unittest.skip("SHIFT NOT IMPLEMENTED")
     def shift_right_test(self):
+        """stim for SRL"""
         pass
 
     def nor_test(self, python=False, verilog=False):
@@ -128,6 +131,137 @@ class TestALU(TestCase):
             self.assertEqual(result, self.result_v)
             self.check_zero(self.z_v, result)
         raise StopSimulation
+
+    def slt_test(self, python=False, verilog=False):
+        """Stim for SLT"""
+        for _ in range(sf['DEFAULT_TEST_LENGTH'] / 10):
+            self.alu_op.next = intbv(0b1001)
+            result = 0 if self.op_1 < self.op_2 else 1
+            yield half_period()
+            if python:
+                self.assertEqual(result, self.result)
+            if verilog:
+                self.assertEqual(result, self.result_v)
+        raise StopSimulation
+
+    def testAddOpPython(self):
+        """Test Add operations Python"""
+        stim = self.add_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testAddOpVerilog(self):
+        """test Add operations Verilog"""
+        stim = self.add_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testAddOpTogether(self):
+        """test add operations Together"""
+        stim = self.add_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testSubOpPython(self):
+        """Test Sub operations Python"""
+        stim = self.sub_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testSubOpVerilog(self):
+        """test Sub operations Verilog"""
+        stim = self.sub_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testSubOpTogether(self):
+        """test Sub operations Together"""
+        stim = self.sub_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testXorOpPython(self):
+        """Test Xor operations Python"""
+        stim = self.xor_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testXorOpVerilog(self):
+        """test Xor operations Verilog"""
+        stim = self.xor_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testXorOpTogether(self):
+        """test Xor operations Together"""
+        stim = self.xor_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testOrOpPython(self):
+        """Test Or operations Python"""
+        stim = self.or_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testOrOpVerilog(self):
+        """test Or operations Verilog"""
+        stim = self.or_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testOrOpTogether(self):
+        """test Or operations Together"""
+        stim = self.or_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testAndOpPython(self):
+        """Test And operations Python"""
+        stim = self.and_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testAndOpVerilog(self):
+        """test And operations Verilog"""
+        stim = self.and_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testAndOpTogether(self):
+        """test And operations Together"""
+        stim = self.and_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testNorOpPython(self):
+        """Test Nor operations Python"""
+        stim = self.nor_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testNorOpVerilog(self):
+        """test Nor operations Verilog"""
+        stim = self.nor_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testNorOpTogether(self):
+        """test Nor operations Together"""
+        stim = self.nor_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
+
+    def testSltOpPython(self):
+        """Test Slt operations Python"""
+        stim = self.slt_test(python=True)
+        Simulation(stim, self.dut).run(quiet=1)
+
+    def testSltOpVerilog(self):
+        """test Slt operations Verilog"""
+        stim = self.slt_test(verilog=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, dut_v).run(quiet=1)
+
+    def testSltOpTogether(self):
+        """test Slt operations Together"""
+        stim = self.slt_test(verilog=True, python=True)
+        dut_v = self.getVerilog()
+        Simulation(stim, self.dut, dut_v).run(quiet=1)
 
 
 if __name__ == '__main__':
