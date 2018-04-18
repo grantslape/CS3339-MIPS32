e71c12d85050c97d6ff0812452f55dff13311975
Grant Slape <me@grantslape.com>
2018-04-17 22:50:54 -0500

    fix ex mem tests

--- test/test_ex_mem.py
+++ test/test_ex_mem.py
@@ -7,11 +7,11 @@ from myhdl import Simulation, StopSimulation
 from src.python.ex_mem import ex_mem, ex_mem_v
 from src.commons.clock import clock_gen
 from src.commons.signal_generator import unsigned_signal_set, signed_signal_set, \
-    random_signed_intbv, random_unsigned_intbv
+    random_signed_intbv, random_unsigned_intbv, rand_unsigned_signal_set, rand_signed_signal_set
 from src.commons.settings import settings as sf
 
 
-@unittest.skip("EX/MEM Register not implemented")
+# @unittest.skip("EX/MEM Register not implemented")
 class TestExMemRegister(TestCase):
     """Testing functionality of EX/MEM Pipeline Register"""
 
@@ -88,8 +88,8 @@ class TestExMemRegister(TestCase):
         """Testing dynamic functionality"""
         for _ in range(sf['DEFAULT_TEST_LENGTH']):
             self.branch_in.next, self.mem_read_in.next, self.reg_write_in.next, \
-                self.z_in.next = randint(0, 1)
-            self.rt_in.next, self.result_in.next = random_signed_intbv()
+                self.z_in.next = rand_unsigned_signal_set(4, width=1)
+            self.rt_in.next, self.result_in.next = rand_signed_signal_set(2)
             self.jmp_addr_in.next = random_unsigned_intbv()
             self.reg_dst_in.next = random_unsigned_intbv(width=5)
             yield self.clock.posedge
