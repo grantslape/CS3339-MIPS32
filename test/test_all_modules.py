3c7b2f4613cd16c6857aa36a6aac16bb12f915d9
Grant Slape <me@grantslape.com>
2018-04-20 00:21:25 -0500

    add test alu back

--- test/test_all_modules.py
+++ test/test_all_modules.py
@@ -8,7 +8,7 @@ from test import test_fwd_unit, test_shift_unit, test_mux32bit2to1, test_pc_adde
     test_mux32bit3to1, test_ex_mux, test_program_counter, test_rfile, test_sign_extender, \
     test_id_ex, test_branch_adder, test_mem_wb, test_ex_mem, test_data_mem, \
     test_branch_unit, test_inst_mem, test_hazard_unit, test_id_shift_left, test_if_id, test_ctrl, \
-    test_ctrl_mux
+    test_ctrl_mux  # , test_alu
 
 MODULES = (test_mux32bit2to1,
            test_mux32bit3to1,
