3b8495d5a5a109f3704f80862225f60e00caaa37
serena-marie <serena-marie@users.noreply.github.com>
2018-04-21 13:12:56 -0500

    wip

--- src/python/alu.py
+++ src/python/alu.py
@@ -1,3 +1,7 @@
+import os
+
+from myhdl import always_comb, Cosimulation
+
 """"
     alu: The main ALU
     input [31:0] op_1: a 32 bit operand.  from alu_mux_1
@@ -6,27 +10,44 @@
     output [31:0] result: a 32 bit result of operation.  to ex_mem
     output z: Zero flag.  to ex_mem
 """""
-def alu(op_1, op_2, alu_op):
-    if(alu_op == 0001): 
-        result.next = op_1 + op2
-    elif(alu_op == 0010	):
-        result.next = op_1 - op_2
-    elif(alu_op == 0011): 
-        result.next = op_1 ^ op_2
-    elif(alu_op == 0100):
-        result.next = op_1 | op_2
-    elif(alu_op == 0101):
-        result.next = op_1 & op_2
-    elif(alu_op == 0110):
-        result.next = op_1 << op_2
-    elif(alu_op == 0111):
-        result.next = op_1 >> op_2
-    elif(alu_op == 1000):
-        result.next = ~(op_1 or op_2)
-    elif(alu_op == 1001):
-        if(op_1 < op_2):
-            result.next = 1
-        else:
-            result.next = 0
 
-    z = true if result.next == 0 else false
+def alu(op_1, op_2, alu_op,z,result):
+   
+    @always_comb 
+    def logic():
+        if(alu_op == 1):
+            result.next = op_1 + op_2
+        elif(alu_op == 2):
+            result.next = op_1 - op_2
+        elif(alu_op == 3): 
+            result.next = op_1 ^ op_2
+        elif(alu_op == 4):
+            result.next = op_1 | op_2
+        elif(alu_op == 5):
+            result.next = op_1 & op_2
+        elif(alu_op == 8):
+            result.next = ~(op_1 or op_2)
+        elif(alu_op == 9):
+            if(op_1 < op_2):
+                result.next = 1
+            else:
+                result.next = 0
+
+        if(result.next == 0):
+            z.next = 1
+        else:
+            z.next = 0
+    return logic
+    
+    #z = true if result.next == 0 else false
+
+def alu_v(op_1, op_2, alu_op, z, result):
+
+    cmd = "iverilog -o bin/alu.out src/verilog/alu.v src/verilog/alu_tb.v"
+    os.system(cmd)
+    return Cosimulation("vvp -m  lib/myhdl.vpi bin/alu.out",
+                         op_1=op_1,
+                         op_2=op_2,
+                         alu_op=alu_op,
+                         z=z,
+                         result=result)
