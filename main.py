468c0434409a2cb63a4fa0910ce66ea9279439d2
Grant Slape <me@grantslape.com>
2018-04-22 11:58:43 -0500

    hold

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 """
 from myhdl import Simulation, bin, StopSimulation
 
+from src.python.inst_mem import inst_mem
 from src.python.program_counter import program_counter
 from src.commons.clock import clock_gen
 from src.commons.settings import settings as sf
@@ -19,7 +20,7 @@ from src.python.mux32bit2to1 import mux32bit2to1
 from src.python.pc_adder import pc_adder
 
 clock, pc_write, pc_src, jmp_ctrl = unsigned_signal_set(4, width=1)
-nxt_inst, cur_pc, imm_jmp_addr, nxt_pc, nxt_inst_mux_a, jmp_addr_last, jmp_reg = unsigned_signal_set(7)
+nxt_inst, cur_pc, imm_jmp_addr, nxt_pc, nxt_inst_mux_a, jmp_addr_last, jmp_reg, inst_out, inst_if = unsigned_signal_set(9)
 
 
 def top():
@@ -36,18 +37,38 @@ def top():
                             out=nxt_inst)
     pc_add = pc_adder(cur_pc=cur_pc,
                       next_pc=nxt_pc)
+    inst_memory = inst_mem(inst_reg=nxt_inst,
+                           inst_out=inst_out)
+    inst_mem_mux = mux32bit2to1(ctrl_line=jmp_ctrl,
+                                input1=inst_out,
+                                input2=Signal(unsigned_intbv()),
+                                out=inst_if)
 
     clock_inst = clock_gen(clock)
 
-    return clock_inst, pc, pc_mux_a, pc_mux_b, pc_add
+    return clock_inst, pc, pc_mux_a, pc_mux_b, pc_add, inst_memory, inst_mem_mux
 
 
 def stim():
     """Test stimulus"""
     cur_pc.next = 4
     while 1:
+        if cur_pc % 50000 == 0:
+            pc_write.next = 1
+            for _ in range(100):
+                yield clock.negedge
+                print("Inst: {}".format(bin(inst_out)))
+            pc_write.next = 0
+            yield clock.negedge
+            print("Inst: {}".format(bin(inst_out)))
         yield clock.negedge
-        print("PC: {}".format(hex(cur_pc)))
+        print("Inst: {}".format(bin(inst_out)))
+
+
+def monitor():
+    """Monitoring output"""
+    yield clock.negedge
+    print("Inst: {}".format(bin(inst_out)))
 
 
 def main():
